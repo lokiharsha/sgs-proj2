13846124c289e1891f14fe2a7186452bebc8bec6
lokiharsha <lakshmipathi.devops76@gmail.com>
2025-04-07 14:47:50 +0530

    modified

--- Test.java
+++ Test.java
@@ -3,6 +3,8 @@ public class Test
     public static void main(String args[])
     {
       System.out.println("hello world");
-System.out.println("manoj");
+      System.out.println("manoj");
+       System.out.println("it's my project");
+
         }
   }
