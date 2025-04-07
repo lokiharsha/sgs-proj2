494075ebe205092e6d6d231809ec9d2887174706
lokiharsha <44457615+lokiharsha@users.noreply.github.com>
2025-04-07 14:52:06 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -3,8 +3,8 @@ public class Test
     public static void main(String args[])
     {
       System.out.println("hello world");
-      System.out.println("manoj");
-       System.out.println("it's my project");
+    
+  System.out.println("it's my project");
 
         }
   }
